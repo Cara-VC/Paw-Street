669d8d0b8a8b60be9629b603b3d555ef73e619c8
An Sun <ansun3366@gmail.com>
2022-12-08 00:47:29 -0500

    Update AWS files

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/pawstreet
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 yum -y install nodejs npm
