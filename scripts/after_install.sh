16efa1312a711ce13d3be8d8ecf3cd39d9583a91
An Sun <ansun3366@gmail.com>
2022-12-08 03:37:35 -0500

    Update scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/pawstreet/front-end
 npm install
 #npm install --save react react-dom react-scripts react-particles-js
-#npm install pm2 -g
+npm install pm2 -g
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,6 +1,6 @@
 cd /home/ec2-user/pawstreet/front-end
 npm start
-# pm2 start npm --name "pawstreet" -- start
-# pm2 startup
-# pm2 save
-# pm2 restart all
+pm2 start npm --name "front-end" -- start
+pm2 startup
+pm2 save
+pm2 restart all
