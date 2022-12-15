9f4e7ecea2efd26767d5b424cd8b122ccbaa34ea
Jiyuan X <jiyuanx2020@163.com>
2022-12-14 22:45:16 -0500

    made edit and delete button work

--- front-end/src/components/EditPost.js
+++ front-end/src/components/EditPost.js
@@ -1,9 +1,9 @@
 import React, {useContext, useEffect, useRef, useState} from 'react';
 import {Container, Form, Button} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 import { getAuth } from "firebase/auth";
-import { Navigate } from "react-router-dom";
+import { Navigate ,useLocation, useNavitate } from "react-router-dom";
 import {AuthContext} from '../firebase/Auth';
 import Profile from "./Profile";
 
@@ -11,107 +11,128 @@ import Profile from "./Profile";
 function EditPost() {
 
     const { currentUser } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [originalData, setOriginalData] = useState();
 
-    // const displayName = useRef(user.displayName);
 
-    // useEffect(() => {
-    //
-    //     async function set() {
-    //         try {
-    //
-    //             await uid.current = user.uid;
-    //
-    //         } catch (e) {
-    //             console.log(e);
-    //         }
-    //     }
-    //
-    //     set();
-    //
-    // }, []);//////////
+    useEffect(() => {
 
-    // console.log(uid.current,1)
-    // console.log(user.uid,2)
+        async function fetchData() {
+            try {
+                await axios.get(
+                    `http://localhost:4000/posts/${location.state.postId}`)
+                    .then(function (response) {
+                        setOriginalData(response.data);
+                    })
+                    .catch(function (error) {
+                        // handle error
+                        console.log(error);
+                    });
 
-    if (currentUser) {
+            } catch (e) {
+                console.log(e);
+            }
+
+        }
+
+        fetchData();
+
+    }, []);
+    if(location.state){
+
+        if(currentUser){
+
+            if(originalData){
+                // console.log(originalData);
+                let temp = originalData.userId;
+                if(currentUser.uid == temp){
+                    return (
+                        <Container>
+                            <h1>Edit Post</h1>
+                            <Form>
+
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Category</Form.Label>
+                                    <Form.Select id="status" defaultValue={originalData.status}>
+                                        <option value="story">Story</option>
+                                        <option value="lost">Lost</option>
+                                        <option value="found">Found</option>
+                                    </Form.Select>
+                                </Form.Group>
+
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Title</Form.Label>
+                                    <Form.Control type="input" placeholder={originalData.title} id="title"/>
+                                </Form.Group>
+
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Pet Name</Form.Label>
+                                    <Form.Control type="input" placeholder={originalData.petName} id="petName"/>
+                                </Form.Group>
+
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Content</Form.Label>
+                                    <Form.Control as="textarea" type="input" placeholder={originalData.content} id="content" />
+                                </Form.Group>
+
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Upload pictures</Form.Label>
+                                    <Form.Control type="file" multiple accept="image/*" id="image"/>
+                                </Form.Group>
+
+                                <Button variant="primary" type="submit" onClick={(e) => {
+
+                                    e.preventDefault();
+
+                                    let newPost = {
+
+                                        title: document.getElementById("title").value != "" ? document.getElementById("title").value : originalData.title,
+                                        status: document.getElementById("status").value != "" ? document.getElementById("status").value : originalData.status,
+                                        content: document.getElementById("content").value != "" ? document.getElementById("content").value : originalData.content,
+                                        image: document.getElementById("image").files != "" ? document.getElementById("image").files : originalData.image,
+                                        petName: document.getElementById("petName").value != "" ? document.getElementById("petName").value : originalData.petName
+
+                                    };
+                                    console.log(newPost);
+
+                                    axios.patch(`http://localhost:4000/posts/${location.state.postId}`,newPost)
+                                        .then(function (response) {
+                                            alert("Successfully edit your post!");
+                                            navigate("/Detail",{state:{postId: response.data._id}});
+                                        })
+                                        .catch(function (error) {
+                                            alert(error);
+                                        });
+                                    // axios.get('http://localhost:4000/posts/')
+                                    //     .then(function (response) {
+                                    //         // handle success
+                                    //         console.log(response);
+                                    //     })
+                                    //     .catch(function (error) {
+                                    //         // handle error
+                                    //         console.log(error);
+                                    //     });
+                                }}>
+                                    Submit
+                                </Button>
+                            </Form>
+                        </Container>
+                    )
+                }
+                // console.log(temp,1);
+
+            }
+        }
+
+
+        // console.log(originalData.userId)
+        // console.log(`http://localhost:4000/posts/${location.state.postId}`)
+        // if(currentUser.uid == originalData.userId)
         // console.log(user.uid,1)
-        return (
-            <div className="container">
-                <h1>Edit Post</h1>
-                <Form>
 
-                    <Form.Group className="mb-3">
-                        <Form.Label>Category</Form.Label>
-                        <Form.Select id="status">
-                            <option value="story">Story</option>
-                            <option value="lost">Lost</option>
-                            <option value="found">Found</option>
-                        </Form.Select>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>Title</Form.Label>
-                        <Form.Control type="input" placeholder="Title" id="title" />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>Pet Name</Form.Label>
-                        <Form.Control type="input" placeholder="Pet Name" id="petName"/>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>Content</Form.Label>
-                        <Form.Control as="textarea" type="input" placeholder="What's new" id="content"/>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>Upload pictures</Form.Label>
-                        <Form.Control type="file" multiple accept="image/*" id="image"/>
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit" onClick={(e) => {
-
-                        e.preventDefault();
-
-                        let newPost = {
-
-                            title: document.getElementById("title").value,
-                            // userId: user.uid,
-                            // userName: user.displayName,
-                            userId:"EtF1bMmYrScbXgLtmtT5NVVEjGi1",
-                            userName:"夏继远",
-                            status: document.getElementById("status").value,
-                            content: document.getElementById("content").value,
-                            image: document.getElementById("image").files,
-                            longitude:-74.0792,
-                            latitude:40.7163,
-                            petName: document.getElementById("petName").value
-                        };
-
-                        axios.patch('http://localhost:4000/posts/',newPost)
-                            .then(function (response) {
-                                console.log(response);
-                            })
-                            .catch(function (error) {
-                                console.log(error);
-                            });
-                        // axios.get('http://localhost:4000/posts/')
-                        //     .then(function (response) {
-                        //         // handle success
-                        //         console.log(response);
-                        //     })
-                        //     .catch(function (error) {
-                        //         // handle error
-                        //         console.log(error);
-                        //     });
-                        console.log(newPost);
-                    }}>
-                        Submit
-                    </Button>
-                </Form>
-            </div>
-        )
     }else {
+        alert("No param passing!")
         return <Navigate to="/signin" />;
     }
 }
